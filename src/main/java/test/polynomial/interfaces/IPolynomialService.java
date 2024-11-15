4e4391efb5643509e7ae101e1952945178fe1581
Andrey Shylnikov <ashylnikov@gmail.com>
2024-11-15 14:49:29 +0200

    added dummy methods for service

--- src/main/java/test/polynomial/interfaces/IPolynomialService.java
+++ src/main/java/test/polynomial/interfaces/IPolynomialService.java
@@ -4,4 +4,18 @@ package test.polynomial.interfaces;
  * Interface used by {@link test.polynomial.controllers.PolynomialController}
  */
 public interface IPolynomialService {
+    /**
+     * Parses given expression
+     * @param expression - string expression to parse
+     * @return parsed result
+     */
+    public String parse(String expression);
+
+    /**
+     * Parses given expression and solve it as function
+     * @param expression - string expression to parse
+     * @param argumentValue - given argument
+     * @return result of function
+     */
+    public int solve(String expression, String argumentValue);
 }
--- src/main/java/test/polynomial/services/PolynomialService.java
+++ src/main/java/test/polynomial/services/PolynomialService.java
@@ -7,4 +7,26 @@ import test.polynomial.interfaces.IPolynomialService;
  */
 @Service
 public class PolynomialService implements IPolynomialService {
+    /**
+     * Parses given expression
+     *
+     * @param expression - string expression to parse
+     * @return parsed result
+     */
+    @Override
+    public String parse(String expression) {
+        return "";
+    }
+
+    /**
+     * Parses given expression and solve it as function
+     *
+     * @param expression    - string expression to parse
+     * @param argumentValue - given argument
+     * @return result of function
+     */
+    @Override
+    public int solve(String expression, String argumentValue) {
+        return 0;
+    }
 }
